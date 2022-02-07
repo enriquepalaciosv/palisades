5540ec90346c98d6eb3b211c094c8547662b5277
Enrique Palacios <enrique.palacios@struck.com>
2022-02-07 11:25:03 -0600

    Set collapsing behavior

--- src/components/TimelineWideSection.vue
+++ src/components/TimelineWideSection.vue
@@ -1,9 +1,30 @@
 <script setup>
 import Content from "./Content.vue";
+import gsap from "gsap";
+import { reactive } from "vue";
+import VueScrollTo from "vue-scrollto";
 // eslint-disable-next-line no-undef
-defineProps({
+const props = defineProps({
   item: { type: Object, default: () => {} },
 });
+
+const state = reactive({
+  fullWidth: false,
+});
+
+const toggle = () => {
+  state.fullWidth = !state.fullWidth;
+  if (state.fullWidth) {
+    gsap.to(".left", { opacity: 0 });
+    gsap.to(".left", { flex: 0 });
+    gsap.to(".left", { display: "none" });
+  } else {
+    gsap.to(".left", { display: "flex" });
+    gsap.to(".left", { opacity: 1 });
+    gsap.to(".left", { flex: 1 });
+  }
+  VueScrollTo.scrollTo(`#tl-section-${props.item.id}`, 1000, {});
+};
 </script>
 
 <template>
@@ -12,7 +33,11 @@ defineProps({
       <div class="left">
         <Content :content="item" :content-index="item.id" />
       </div>
-      <div class="right">right</div>
+      <div class="right" :class="`tl-section-${item.id}`">
+        <button class="toggler" @click="toggle()">
+          <img src="../assets/images/fullscreen.svg" alt="expand/collapse" />
+        </button>
+      </div>
     </div>
   </section>
 </template>
@@ -27,8 +52,19 @@ defineProps({
       @apply flex-1 flex justify-center items-center;
     }
     .right {
-      @apply flex-1 flex bg-orange;
+      @apply flex-1;
       height: 100vh;
+      position: relative;
+      &.tl-section-7 {
+        background-image: url("../assets/images/section-7.png");
+        background-size: cover;
+        background-position: center;
+      }
+      .toggler {
+        position: absolute;
+        bottom: 20px;
+        left: 32px;
+      }
     }
   }
 }
