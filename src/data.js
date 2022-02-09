a890f461a9ab85626819c2ecbd88f7fd3e475ef4
Enrique Palacios <enrique.palacios@struck.com>
2022-02-09 11:54:01 -0600

    Set full data

--- src/data.js
+++ src/data.js
@@ -15,7 +15,7 @@ const firstBundle = [
         id: 2,
         year: 1939,
         title: "1939. The Poulsens and the Cushings",
-        description: "Originally from the Bay Area, Wayne Poulsen spends his youth ski jumping and touring in the Sierra Nevada, working with Dr. Frank Church as a snow hydrographer, jumping in early ski competitions, and eventually helping to start the University of Nevada, Reno’s ski team. After helping to start the ski area now known as Sky Tavern, Poulsen ventured further into the Sierra and fell in love with the stunning valley known as Squaw Valley. In 1939, Poulsen secures an option to buy land in the valley from the Southern Pacific Railroad, and by the mid-1940s, together with his wife, Sandy, the Poulsens build a home to raise their family and pursue their dream of developing Squaw Valley. Poulsen meets Alexander Cushing, an East Coast businessman, while on a ski trip at Alta in 1945, and the two partner, with Poulsen as the expert and Cushing as the financier.",
+        description: "Originally from the Bay Area, Wayne Poulsen spends his youth ski jumping and touring in the Sierra Nevada, working with Dr. Frank Church as a snow hydrographer, jumping in early ski competitions, and eventually helping to start the University of Nevada, Reno's ski team. After helping to start the ski area now known as Sky Tavern, Poulsen ventured further into the Sierra and fell in love with the stunning valley known as Squaw Valley. In 1939, Poulsen secures an option to buy land in the valley from the Southern Pacific Railroad, and by the mid-1940s, together with his wife, Sandy, the Poulsens build a home to raise their family and pursue their dream of developing Squaw Valley. Poulsen meets Alexander Cushing, an East Coast businessman, while on a ski trip at Alta in 1945, and the two partner, with Poulsen as the expert and Cushing as the financier.",
     },
     {
         id: 3,
@@ -36,17 +36,37 @@ const firstBundle = [
     {
         id: 4,
         year: 1960,
-        title: "1960"
+        title: "1960. Winter Olympic Games",
+        description: "Cushing pulls off the triumph of a lifetime by convincing a very skeptical International Olympic Committee to award Squaw Valley the first Winter Olympic Games to be held in the western United States. The games are the first to be nationally televised, giving many people their first look at the exotic sport of skiing, and kicks off a wave of national interest in the sport. Inside the Olympic Village Lodge, athletes from every country dine together—the first and last time in Olympic history. Walt Disney leads the opening ceremonies, the sports of biathlon and women's speed skating are introduced, and metal skis are used for the first time in ski racing. Computers are utilized for the first time to compute the scores, and the U.S. men's ice hockey team beats the Russians in a major upset that's been called the first miracle on ice."
+
     },
     {
         id: 5,
         year: 1961,
-        title: "1961"
+        items: [
+            {
+                itemTitle: "1961. Alpine Meadows Opens",
+                itemDescription: "On December 28, 1961, skiers ride to the top of Ward Peak at the newly-opened Alpine Meadows, on a lift called Chair One, a zone now accessed by the six-person Summit Express. By 1965, Alpine Meadows quadruples its terrain, adding six lifts and opening access to Scott Peak and the backside of Ward Peak, encompassing 2,400 skiable acres."
+            },
+            {
+                itemTitle: "1968. Aerial Tram Installed",
+                itemDescription: "A cable car is shipped from Switzerland and constructed and installed at Squaw Valley. It whisks skiers 1.5 miles up a steep rock face to a mountaintop lodge called High Camp. Engineers looked at trams across Europe to consider the feasibility of designing one here. At the time, it is the largest aerial tramway in the world."
+            },
+            {
+                itemTitle: "Late 1960s. Creating Champions",
+                itemDescription: ""
+            },
+            {
+                itemTitle: "1970. Community of Writers at Squaw Valley",
+                itemDescription: ""
+            }
+        ]
     },
     {
         id: 6,
         year: 1975,
-        title: "1975"
+        title: "1975. Palisades Put On the Map",
+        description: "Tahoe filmmaker Craig Beck debuts his film, Daydreams, shot on location at Squaw Valley. The movie is the first to capture high-stakes skiing off Squaw's Palisades, the precipitous cliff band adjacent to the start of the 1960 men's Olympic Downhill. In the film, Craig's younger brother, Greg, airs over 100 feet to a stomped landing (on what is now known as Beck's Rock) and Dave Burnham pulls off an 80-foot flip, giving the Palisades its modern-day mantra: “When it doubt, air it out.” By the early 80s, Warren Miller comes to Squaw Valley to film the likes of Scot Schmidt, Tom Day, Jim Zellers, and others, cementing Squaw's reputation as an extreme skier's stomping grounds."
     },
 ];
 
@@ -54,7 +74,6 @@ const firstWide =
 {
     id: 7,
     year: 1979,
-    title: "1979",
     items: [
         {
             itemTitle: "1975. The Controversy of Silverado Lift",
@@ -76,17 +95,41 @@ const secondBundle = [
     {
         id: 8,
         year: 1988,
-        title: "1988"
+        title: "1988. The Dawn of Film Skiing",
+        description: "The rock-punk ski movie Blizzard of Aahhh's, by Greg Stump, celebrates the new era of extreme skiing, starring then virtually unknown skiers Glen Plake, Scot Schmit, and Mike Hattrup flying over cliffs at Squaw Valley in brightly-colored one-pieces. The film is just one of many classic ski and snowboard movies filmed on these hallowed slopes, including Warren Miller's Steep and Deep (1985), Hollywood's Hot Dog...The Movie (1984), Standard Films' first snowboard movie Totally Board (1991), Scott Gaffney's first film Walls of Freedom (1995), and countless Matchstick Productions, Poor Boyz, and Teton Gravity Research movies of the '90s and 2000s that gave the mountain its nickname “Squallywood.”",
+        items: [
+            {
+                itemTitle: "1990. Cushing Crossing Pond Skimming",
+                itemDescription: "Lorem Ipsum"
+            }
+        ]
     },
     {
         id: 9,
         year: 1991,
-        title: "1991"
+        title: "1991. Jerry Garcia Band Plays",
+        description: "Concert promoter Bill Graham teams up with Squaw Valley to present the Squaw Valley Summer Music Festival. On the lineup: the Jerry Garcia Band, Béla Fleck and the Flecktones, David Grisman, and others. It's the first of many major music festivals and acts to take the stage at the base of the legendary ski resort and a predecessor to events like the modern-day WinterWondergrass Festival.",
+        items: [
+            { itemTitle: "1998. Opening of the Gold Coast Funitel ", itemDescription: "Lorem Ipsum" }
+        ]
     },
     {
         id: 10,
         year: 1998,
-        title: "1988"
+        items: [
+            {
+                itemTitle: "1998. Jonny Moseley Wins Olympic Gold",
+                itemDescription: "Concert promoter Bill Graham teams up with Squaw Valley to present the Squaw Valley Summer Music Festival. On the lineup: the Jerry Garcia Band, Béla Fleck and the Flecktones, David Grisman, and others. It's the first of many major music festivals and acts to take the stage at the base of the legendary ski resort and a predecessor to events like the modern-day WinterWondergrass Festival."
+            },
+            {
+                itemTitle: "1999. C.R. Johnson Lands a 1440",
+                itemDescription: "Lorem Ipsum"
+            },
+            {
+                itemTitle: "2001. Shane McConkey Conquers the World",
+                itemDescription: "Lorem Ipsum"
+            }
+        ]
     }];
 
 
@@ -94,30 +137,75 @@ const wideBundle = [
     {
         id: 11,
         year: 2002,
-        title: "2002"
+        items: [
+            {
+                itemTitle: "2002. The Village at Squaw Valley",
+                itemDescription: "Lorem Ipsum"
+            },
+            {
+                itemTitle: "2003. Squallywood The Book Published",
+                itemDescription: "Lorem Ipsum"
+            },
+            {
+                itemTitle: "2006. Julia Mancuso Wins First Olympic Gold",
+                itemDescription: "Lorem Ipsum"
+            },
+            {
+                itemTitle: "2006. Mike Wilson's Double Backflips",
+                itemDescription: "Lorem Ipsum"
+            }
+        ]
     },
     {
         id: 12,
         year: 2011,
-        title: "2011"
+        title: "2011. Squaw Valley and Alpine Meadows Unite",
+        description: "After many years of separate ownership, Squaw Valley and Alpine Meadows join forces in November 2011. Together, the two ski areas offer skiers and riders access to over 6,000 acres on a single lift ticket and season pass and reignite John Reilly's dreams of a lift connection between the two ski areas."
     },
     {
         id: 13,
         year: 2016,
-        title: "2016"
+        items: [
+            {
+                itemTitle: "2016. Backcountry Tours Out the Gates",
+                itemDescription: "Lorem Ipsum"
+            },
+            {
+                itemTitle: "2016. Spring Skiing Capital",
+                itemDescription: "Lorem Ipsum"
+            }
+        ]
     }]
 
 const lastSingle =
 {
     id: 14,
     year: 2017,
-    title: "2017"
+    title: "2017. Squaw Valley Hosts World Cup",
+    description: "Squaw Valley hosts a women’s World Cup race, the first time the mountain staged a World Cup in 19 years. The U.S. Ski and Snowboard Association also designates Squaw Valley Alpine Meadows an official training site. Squaw Valley Alpine Meadows has sent 64 athletes (and counting) to the U.S. Ski and Snowboard Team and more than two dozen athletes to the Olympics, including legendary names like Eric, Sandra, and Wayne Poulsen, Jimmie Heuga, Bill Hudson, Tamara McKinney, Nate Holland, Daron Rahlves, Shannon Bahrke, Jonny Moseley, Julia Mancuso, Marco Sullivan, Travis Ganong, and more."
 };
 const lastWide =
 {
     id: 15,
     year: 2018,
-    title: "2018"
+    items: [
+        {
+            itemTitle: "2018. Introducing the Ikon Pass",
+            itemDescription: "Lorem Ipsum"
+        },
+        {
+            itemTitle: "2019. Treeline Cirque Provides Access to the Backside",
+            itemDescription: "Lorem Ipsum"
+        },
+        {
+            itemTitle: "2021. Squaw Valley Renamed TK",
+            itemDescription: "Lorem Ipsum"
+        },
+        {
+            itemTitle: "2021. Base-to-Base Gondola Installed",
+            itemDescription: "Lorem Ipsum"
+        }
+    ]
 };
 
 const navigation = [...firstBundle, { ...firstWide }, ...secondBundle, ...wideBundle, { ...lastSingle }, { ...lastWide }];
