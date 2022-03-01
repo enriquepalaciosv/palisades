0a89c7bc658712b7cc4b24255680e3dcb08fd854
Enrique Palacios <enrique.palacios@struck.com>
2022-03-01 15:29:46 -0600

    Update content

--- src/data.js
+++ src/data.js
@@ -2,9 +2,9 @@ const firstBundle = [
   {
     id: 1,
     year: 0,
-    title: "9,000 Years Ago: The Area's First Inhabitants",
+    title: "9,000 Years Ago. The Area’s First Inhabitants",
     description:
-      "An indigenous Native American people, the Washoe Tribe inhabited lands around Lake Tahoe dating back thousands of years. They hunted, gathered, and traveled on hand-hewn snowshoes in the winter months. Their tribe name comes from the Washoe word, Wašiw, meaning “people from here.” The Washoe first called Lake Tahoe “Da ow ga,” the Washoe word for “lake,” which is thought to be the source for the name Lake Tahoe. Da ow aga is central to the tribe's identity and is still considered the tribe's most sacred place.",
+      "An indigenous Native American people, the Washoe Tribe inhabited lands around Lake Tahoe dating back thousands of years. They hunted, gathered, and traveled on hand-hewn snowshoes in the winter months. Their tribe name comes from the Washoe word, Wašiw, meaning “people from here.” The Washoe first called Lake Tahoe “Da ow ga,” the Washoe word for “lake,” which is thought to be the source for the name Lake Tahoe. Da ow aga is central to the tribe’s identity and is still considered the tribe’s most sacred place.",
     caption: "PHOTO CAPTION",
     author: "Name Name | Name Name",
     items: [
@@ -21,7 +21,7 @@ const firstBundle = [
     year: 1939,
     title: "1939. The Poulsens and the Cushings",
     description:
-      "Originally from the Bay Area, Wayne Poulsen spends his youth ski jumping and touring in the Sierra Nevada, working with Dr. Frank Church as a snow hydrographer, jumping in early ski competitions, and eventually helping to start the University of Nevada, Reno's ski team. After helping to start the ski area now known as Sky Tavern, Poulsen ventured further into the Sierra and fell in love with the stunning valley known as Squaw Valley. In 1939, Poulsen secures an option to buy land in the valley from the Southern Pacific Railroad, and by the mid-1940s, together with his wife, Sandy, the Poulsens build a home to raise their family and pursue their dream of developing Squaw Valley. Poulsen meets Alexander Cushing, an East Coast businessman, while on a ski trip at Alta in 1945, and the two partner, with Poulsen as the expert and Cushing as the financier.",
+      "Originally from the Bay Area, Wayne Poulsen spends his youth ski jumping and touring in the Sierra Nevada, working with Dr. Frank Church as a snow hydrographer, jumping in early ski competitions, and eventually helping to start the University of Nevada, Reno’s ski team. After helping to start the ski area now known as Sky Tavern, Poulsen ventured further into the Sierra and fell in love with the stunning valley known as Squaw Valley. In 1939, Poulsen secures an option to buy land in the valley from the Southern Pacific Railroad, and by the mid-1940s, together with his wife, Sandy, the Poulsens build a home to raise their family and pursue their dream of developing Squaw Valley. Poulsen meets Alexander Cushing, an East Coast businessman, while on a ski trip at Alta in 1945, and the two partner, with Poulsen as the expert and Cushing as the financier.",
     caption: "PHOTO CAPTION 2",
     author: "Name Name | Name Name 2",
   },
@@ -62,13 +62,10 @@ const firstBundle = [
     year: 1961,
     caption: "PHOTO CAPTION",
     author: "Name Name | Name Name",
+    title: "1961. Alpine Meadows Opens",
+    description:
+      "On December 28, 1961, skiers ride to the top of Ward Peak at the newly-opened Alpine Meadows, on a lift called Chair One, a zone now accessed by the six-person Summit Express. By 1965, Alpine Meadows quadruples its terrain, adding six lifts and opening access to Scott Peak and the backside of Ward Peak, encompassing 2,400 skiable acres.",
     items: [
-      {
-        itemImage: "section-5-2.png",
-        itemTitle: "1961. Alpine Meadows Opens",
-        itemDescription:
-          "On December 28, 1961, skiers ride to the top of Ward Peak at the newly-opened Alpine Meadows, on a lift called Chair One, a zone now accessed by the six-person Summit Express. By 1965, Alpine Meadows quadruples its terrain, adding six lifts and opening access to Scott Peak and the backside of Ward Peak, encompassing 2,400 skiable acres.",
-      },
       {
         itemImage: "section-5-3.png",
         itemTitle: "1968. Aerial Tram Installed",
@@ -78,12 +75,14 @@ const firstBundle = [
       {
         itemImage: "section-5-4.png",
         itemTitle: "Late 1960s. Creating Champions",
-        itemDescription: "",
+        itemDescription:
+          "A first-of-its-kind kids’ ski program called Mighty Mites is introduced at Squaw Valley, enabling youngsters starting at age five to join a youth ski development program. Since then, over 50 Mighty Mites have gone on to the U.S. Ski Team and at least 20 of them have competed in the Olympics. Athletes including Daron Rahlves, Julia Mancuso, C.R. Johnson, Cody Townsend, Travis Gangong, Amie Engerbretson, Michelle Parker, and JT Holmes got their start as Mighty Mites.",
       },
       {
         itemImage: "section-5-5.png",
         itemTitle: "1970. Community of Writers at Squaw Valley",
-        itemDescription: "",
+        itemDescription:
+          "Writers Oakley Hall and Blair Fuller start what is now the oldest writer’s conference on the west coast. Over the next 50 years, the conference becomes one of the most important in the U.S. Alumni include poet laureates of the United States as well as several individual states, numerous Pulitzer Prize and National Book Award winners, and active poets, screenwriters, novelists, and university faculty from around the world.",
       },
     ],
   },
@@ -100,25 +99,28 @@ const firstBundle = [
 
 const firstWide = {
   id: 7,
-  year: 1979,
+  year: 1975,
   caption: "PHOTO CAPTION",
   author: "Name Name | Name Name",
-  expandable: true,
+  expandable: false,
   items: [
     {
-      itemImage: "section-7-2.png",
+      itemImage: "section-6.png",
       itemTitle: "1975. The Controversy of Silverado Lift",
-      itemDescription: "Lorem Ipsum",
+      itemDescription:
+        "A new master plan for Squaw Valley includes the construction of two new chairlifts above Shirley Canyon: Solitude and Silverado. Over the ensuing years, conflict arises over the building of Silverado in an area many consider to be pristine wilderness, but Cushing presses on. In 1989, Cushing orders the project to move forward, and a large stand of old-growth forest is cut down over one weekend without regulatory approval. The ensuing lawsuits and controversy both burnish and tarnish Cushing’s reputation, incurring penalties and court-imposed limits that result in operational compromises to this day. Silverado did become a legendary lift, with steep cliffs and chutes accessible only via designated gates and only during its limited operating season.",
     },
     {
-      itemImage: "section-7-3.png",
-      itemTitle: "1979. The First Halfpipe",
-      itemDescription: "Lorem Ipsum",
-    },
-    {
-      itemImage: "section-7-4.png",
+      itemImage: "section-6.png",
       itemTitle: "1982. Avalanche at Alpine Meadows",
-      itemDescription: "Lorem Ipsum",
+      itemDescription:
+        "On March 31, 1982, a late-season storm dumps nearly 90 inches of snow in four days. Avalanche conditions are so extreme that Alpine Meadows mountain manager Bernie Kingery closes the mountain to everyone, including employees. When a series of massive avalanches break along Alpine’s northeast ridge, it destroys the Summit Terminal building, where Kingery and others are holding up, as well as portions of the parking lot. Seven people, including Kingery, are tragically killed. One employee, Anna Conrad, survives after spending five days buried in a locker room. To date, Squaw Valley Alpine Meadows has one of the most extensive avalanche mitigation regimens of any ski resort in the world, but the threat of avalanche remains ever-present.",
+    },
+    {
+      itemImage: "section-7-5.png",
+      itemTitle: "1986-1996.  Ski Resorts Open Doors to Snowboarders",
+      itemDescription:
+        "On March 31, 1982, a late-season storm dumps nearly 90 inches of snow in four days. Avalanche conditions are so extreme that Alpine Meadows mountain manager Bernie Kingery closes the mountain to everyone, including employees. When a series of massive avalanches break along Alpine’s northeast ridge, it destroys the Summit Terminal building, where Kingery and others are holding up, as well as portions of the parking lot. Seven people, including Kingery, are tragically killed. One employee, Anna Conrad, survives after spending five days buried in a locker room. To date, Squaw Valley Alpine Meadows has one of the most extensive avalanche mitigation regimens of any ski resort in the world, but the threat of avalanche remains ever-present.",
     },
   ],
 };
@@ -136,7 +138,8 @@ const secondBundle = [
       {
         itemImage: "section-8-2.png",
         itemTitle: "1990. Cushing Crossing Pond Skimming",
-        itemDescription: "Lorem Ipsum",
+        itemDescription:
+          "The inaugural Cushing Crossing Pond Skimming event takes place at Squaw Valley as one of the nation’s original pond skimming competitions. It sets in motion a spirited, spring skiing tradition that lives on to this day.",
       },
     ],
   },
@@ -152,7 +155,8 @@ const secondBundle = [
       {
         itemImage: "section-9-2.png",
         itemTitle: "1998. Opening of the Gold Coast Funitel ",
-        itemDescription: "Lorem Ipsum",
+        itemDescription:
+          "North America’s only Funitel opens at Squaw Valley, with each of its cabins carrying up to 28 passengers. Traveling at speeds up to 20 feet per second, the Funitel carries guests to the Gold Coast Lodge on the upper mountain in under eight minutes and can transport up to 4,000 people per hour.",
       },
     ],
   },
@@ -163,20 +167,22 @@ const secondBundle = [
     author: "Name Name | Name Name",
     items: [
       {
-        itemImage: "section-10-2.png",
+        itemImage: "section-10.png",
         itemTitle: "1998. Jonny Moseley Wins Olympic Gold",
         itemDescription:
-          "Concert promoter Bill Graham teams up with Squaw Valley to present the Squaw Valley Summer Music Festival. On the lineup: the Jerry Garcia Band, Béla Fleck and the Flecktones, David Grisman, and others. It's the first of many major music festivals and acts to take the stage at the base of the legendary ski resort and a predecessor to events like the modern-day WinterWondergrass Festival.",
+          "Jonny Moseley, who got his start as a young Mighty Mite at Squaw Valley, wins gold in mogul skiing at the Nagano Winter Olympics with a groundbreaking 360 mute grab. Four years later, at the 2002 Olympics, Moseley lands a progressive new off-axis trick he invented, called the Dinner Roll, which changes the scope of freestyle skiing and is routinely done in mogul competitions to this day.",
       },
       {
-        itemImage: "section-10-3.png",
+        itemImage: "section-10.png",
         itemTitle: "1999. C.R. Johnson Lands a 1440",
-        itemDescription: "Lorem Ipsum",
+        itemDescription:
+          "From Mighty Mites to pro skier, C.R. Johnson grew up on the slopes of Squaw Valley and was known for his progressive style and positive energy. At age 15, he becomes the first skier to land a 1440—four complete spins—a trick captured on film at Squaw Valley in Teton Gravity Research’s film Area 51. Johnson, beloved to this day, died in a tragic fall in 2010 at the age of 26.",
       },
       {
-        itemImage: "section-10-4.png",
+        itemImage: "section-10.png",
         itemTitle: "2001. Shane McConkey Conquers the World",
-        itemDescription: "Lorem Ipsum",
+        itemDescription:
+          'After years of hucking himself off of the gnarliest cliffs Squaw Valley has to offer, Shane McConkey lands a trifecta of acknowledgements: ESPN’s Action Sports Skier of the Year, first place in Red Bull’s Ultra Cross Big Air Comp, and Skiing Magazine’s #1 spot in the Top 25 skiers in North America. Forever progressing the sport through his wild skills and carefree spirit, Shane also finds time to invent the reverse camber and reverse sidecut ski, changing powder skiing forever. When he is tragically killed while BASE jumping in the Dolomites in 2009, an eagle sculpted from steel is bolted to the top of KT-22 to honor his memory and to inspire the ski pioneers of the future. Under the leadership of his wife, Sherry, the Shane McConkey Foundation continues to raise money for local schools and organizations, and the Pain McShlonkey Classic brings a celebratory vibe to the slopes each spring. To learn more about Shane & the freeski movement check out our "Land of Freeski" story here. ',
       },
     ],
   },
@@ -188,27 +194,31 @@ const wideBundle = [
     year: 2002,
     caption: "PHOTO CAPTION",
     author: "Name Name | Name Name",
-    expandable: true,
+    expandable: false,
     items: [
       {
         itemImage: "section-11-2.png",
         itemTitle: "2002. The Village at Squaw Valley",
-        itemDescription: "Lorem Ipsum",
+        itemDescription:
+          "Construction for the new base village at Squaw Valley begins, with the addition of lodging, restaurants, and shops at the base of the world-class ski resort.",
       },
       {
         itemImage: "section-11-3.png",
         itemTitle: "2003. Squallywood The Book Published",
-        itemDescription: "Lorem Ipsum",
+        itemDescription:
+          "Extreme skier Robb Gaffney writes and publishes a book, Squallywood: A Guide to Squaw Valley’s Most Exposed Lines. The book brings attention to some of the flashier and lesser-known lines around Squaw Valley, the steep chutes that have served as testing grounds for some of the most legendary big-mountain freeskiers and snowboarders, including Jeremy Jones, Shane McConkey, Michelle Parker, Jamie Burge, Kent Kreitler, Timy Dutton, CR Johnson, JT Holmes, Ingrid Backstrom, Cody Townsend, Elyse Saugstad, and more.",
       },
       {
         itemImage: "section-11-4.png",
         itemTitle: "2006. Julia Mancuso Wins First Olympic Gold",
-        itemDescription: "Lorem Ipsum",
+        itemDescription:
+          "Growing up on the slopes of Squaw Valley, ski racer Julia Mancuso joined the U.S. Ski Team as a teenager. By 2006, she wins her first Olympic gold in giant slalom at the Torino Winter Games. She goes on to become one of the most decorated American ski racers of all time, with four Olympic medals, five World Championship medals, and seven World Cup victories.",
       },
       {
         itemImage: "section-11-5.png",
         itemTitle: "2006. Mike Wilson's Double Backflips",
-        itemDescription: "Lorem Ipsum",
+        itemDescription:
+          "Pro skier and big air specialist Mike Wilson throws consecutive double backflips off Beck’s Rock in the Palisades, 31 years after Greg Beck gave the rock its name (see: 1975). Wilson’s triumph is captured in Matchstick Productions’ movie The Push.",
       },
     ],
   },
@@ -232,12 +242,14 @@ const wideBundle = [
       {
         itemImage: "section-13-2.png",
         itemTitle: "2016. Backcountry Tours Out the Gates",
-        itemDescription: "Lorem Ipsum",
+        itemDescription:
+          "As a result of US Forest Service regulatory concerns, Squaw Valley has always kept its backcountry gates closed, but starting in 2016, Alpenglow Expeditions, a permitted guide service, partners with Squaw Valley Alpine Meadows to offer lift-accessed guided backcountry tours out the gates into Squaw Valley’s prized, rarely-skied zones like National Geographic Bowl and Tram Ridge. The partnership later leads to the construction of the Tahoe Via, a via ferrata on the iconic Tram Face.",
       },
       {
         itemImage: "section-13-3.png",
         itemTitle: "2016. Spring Skiing Capital",
-        itemDescription: "Lorem Ipsum",
+        itemDescription:
+          "Squaw Valley Alpine Meadows cements its status as Spring Skiing Capital: the official headquarters of spring skiing fun. The following year, at the end of a big winter, the inaugural Freedom Fest celebrates skiing at Squaw Valley on the Fourth of July.",
       },
     ],
   },
@@ -257,27 +269,31 @@ const lastWide = {
   year: 2018,
   caption: "PHOTO CAPTION",
   author: "Name Name | Name Name",
-  expandable: true,
+  expandable: false,
   items: [
     {
       itemImage: "section-15-2.png",
       itemTitle: "2018. Introducing the Ikon Pass",
-      itemDescription: "Lorem Ipsum",
+      itemDescription:
+        "In January 2018, the newly formed Alterra Mountain Company announces a new collective season pass, called the Ikon Pass, that offers access to 23 ski resorts across nine states and three Canadian provinces, including unlimited access to Squaw Valley Alpine Meadows. The number of destinations on the Ikon Pass has continued to grow since then.",
     },
     {
       itemImage: "section-15-3.png",
       itemTitle: "2019. Treeline Cirque Provides Access to the Backside",
-      itemDescription: "Lorem Ipsum",
+      itemDescription:
+        "Alpine Meadows opens Treeline Cirque, a new high-speed quad chairlift that transports skiers and riders to the backside of the mountain in five minutes, a journey that previously took multiple chairlifts or a hike over the High Traverse. That year, 2019, also brings a record-breaking snowfall in February: The mountains receive over 313 inches of snow that month alone, a new monthly record that coins the term #februBURIED.",
     },
     {
       itemImage: "section-15-4.png",
-      itemTitle: "2021. Squaw Valley Renamed TK",
-      itemDescription: "Lorem Ipsum",
+      itemTitle: "Squaw Valley Renamed Palisades Tahoe",
+      itemDescription:
+        "After many years of considering a name change due to the etymology and history of the term “squaw,” which is deemed a derogatory and offensive term to Native American women, the resort commences on a name changing initiative, which involves extensive community feedback and working with the local Washoe Tribe, and announces its new name: TK.",
     },
     {
       itemImage: "section-15-5.png",
       itemTitle: "2021. Base-to-Base Gondola Installed",
-      itemDescription: "Lorem Ipsum",
+      itemDescription:
+        "Construction begins on the historic base-to-base gondola that will at long last connect the two neighboring resorts. The gondola will travel roughly 2.2 miles in approximately 15 minutes and will include two mid-stations, including one at the top of KT-22.",
     },
   ],
 };
